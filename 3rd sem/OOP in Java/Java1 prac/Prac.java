8c2bf5df9f8e07c45259e6da14c0ee3d95913829
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2022-08-23 18:05:46 +0545

    add .gitignore & java practice files

---
+++ 3rd sem/OOP in Java/Java1 prac/Prac.java
@@ -0,0 +1,9 @@
+public class Prac 
+{
+    public static void main(String arr[]) 
+    {
+        System.out.println("Hello new world.");
+        System.out.println("Slow shit");
+        System.out.println("Okay-ish");
+    }
+}
