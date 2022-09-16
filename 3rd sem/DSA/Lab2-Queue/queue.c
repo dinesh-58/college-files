77a1383191f8a1e6881034a5f60775163fd801d4
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2022-09-16 19:23:37 +0545

    change function for getting form values

--- 3rd sem/DSA/Lab2-Queue/queue.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void main() {
-    char infix[20];
-    printf("Enter infix expression");
-    scanf(" %[^\n]",infix);
-    printf("String was %s",infix);   
-}
