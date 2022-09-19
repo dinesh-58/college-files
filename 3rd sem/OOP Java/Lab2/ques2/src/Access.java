04507cf4b12408f8ffef2c0c0dc1384589c16e32
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2022-09-19 09:37:52 +0545

    add files for java lab2/ques2

---
+++ 3rd sem/OOP Java/Lab2/ques2/src/Access.java
@@ -0,0 +1,4 @@
+public class Access {
+    public String name;
+    // just write in theory that if this was private, error would occur in main method.
+}
---
+++ 3rd sem/OOP Java/Lab2/ques2/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Access demo = new Access();
+        demo.name = "Sujal";
+        System.out.println("My name is "+demo.name);
+    }
+}
