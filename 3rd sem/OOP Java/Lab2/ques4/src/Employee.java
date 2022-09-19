1d610845d06a392b1db3dded641f095d50705613
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2022-09-19 17:21:04 +0545

    shorten code for java2/ques4

--- 3rd sem/OOP Java/Lab2/ques4/src/Employee.java
+++ 3rd sem/OOP Java/Lab2/ques4/src/Employee.java
@@ -8,12 +8,15 @@ public class Employee {
         name = "Ram";
         joinedYear = 2056;
         address = "Kathmandu";
+        System.out.println(name+" "+joinedYear+" "+address);
     }
     // parameterized constructor
     public Employee (String name, int joinedYear, String address) {
         this.name = name;
         this.joinedYear = joinedYear;
         this.address = address; 
+        System.out.println(name+" "+joinedYear+" "+address);
+
     }
 
     
