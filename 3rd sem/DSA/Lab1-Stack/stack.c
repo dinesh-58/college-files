509373085a69697992fa3ca3488cb6b667699c53
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2022-10-20 14:43:24 +0545

    update stack program to have separate functions

--- 3rd sem/DSA/Lab1-Stack/stack.c
+++ 3rd sem/DSA/Lab1-Stack/stack.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SIZE 5
+int stack[SIZE], top=-1;
+
+void push();
+void pop();
+void disp();
+
 void main() {
-    int n=5, stack[n], i, value, choice, top=-1, temp;
-    for (i=0; i<n; i++) {
+    int i, choice;
+
+    // initialize all values of stack
+    for (i=0; i<SIZE; i++) {
         stack[i] = 0;
     }
     do {
@@ -12,33 +21,48 @@ void main() {
         printf("\n 2: POP 1 value");
         printf("\n 3: exit \n");
         scanf("%d",&choice);
+        
+          choice == 1? push()    
+        : choice == 2? pop()
+        : exit(0);
 
-        if (choice == 1) { // PUSH
-            if (top == n-1) {
-                printf("Overflow");
-                exit(1);
-            } 
-            printf("Enter numeric value to push");
-            scanf("%d", &value);
-            top++;
-            stack[top] = value;
-        }    
-        else if (choice == 2) { // POP
-            if (top == -1) {
-                printf("Underflow");
-                exit(1);
-            }
-            temp = stack[top];
-            printf("The popped value is %d", temp);
-            stack[top] = 0;
-            top--;
-        }
-        else exit(0);
-        // display current Stack status  
-        printf("Index \t Value");
-        for (i=n-1; i>=0; i--) {
-            printf("\n %d \t %d",i ,stack[i]);
-            if (i==top) printf("\t <--- TOP");
-        }
+        disp();
     } while(1);
+}
+
+void push() {
+    int value;
+    if (top == SIZE-1) {
+        printf("Overflow");
+        exit(1);
+    }
+    // no need for else because program exits if condition met 
+    printf("Enter numeric value to push ");
+    scanf("%d", &value);
+    top++;
+    stack[top] = value;
+}
+
+void pop() {
+    int temp;
+    if (top == -1) {
+        printf("Underflow");
+        exit(1);
+    }
+
+    // no need for else because program exits if condition met 
+    temp = stack[top];
+    printf("The popped value is %d \n", temp);
+    stack[top] = 0;
+    top--;
+}
+
+void disp() {
+    // displays current stack values 
+    int i;
+    printf("Index \t Value");
+    for (i=SIZE-1; i>=0; i--) {
+        printf("\n %d \t %d",i ,stack[i]);
+        if (i==top) printf("\t <--- TOP");
+    }
 }
