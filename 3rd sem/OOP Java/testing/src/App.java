30cbbe8553482aec9169ed3602173c17e153a44b
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2022-11-01 14:36:13 +0545

    delete cached files in testing directory

--- 3rd sem/OOP Java/testing/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println(StaticClass.var);
-        StaticClass.var++;
-        StaticClass.disp();
-    }
-}
--- 3rd sem/OOP Java/testing/src/StaticClass.java
+++
@@ -1,7 +0,0 @@
-public class StaticClass {
-    public static int var =10;
-
-    public static void disp() {
-        System.out.println(var);
-    }
-}
