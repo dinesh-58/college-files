8175e6a8ffac40b42ed63321e52de3a5124a2ec8
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2022-11-21 20:32:04 +0545

    fix recursive-fibo.c

--- 3rd sem/DSA/recursive-fibo.c
+++ 3rd sem/DSA/recursive-fibo.c
@@ -1,16 +1,21 @@
 #include <stdio.h> 
 
-int fibo(int n) {
+int fibo(int n, int disp) {
     int temp;
     if (n==1) {
+        if (disp==1) {
+            printf("1 \t");
+        }
         return 1; 
     } 
-    if (n==0) { 
+    else if (n<=0) { 
         return 0;
     }
-    //else 
-    temp = fibo(n-1)+fibo(n-2);
-    printf("%d \t", temp);
+    if (disp == 1) {
+        temp = fibo(n-1, 1)+fibo(n-2, 0);
+        printf("%d \t", temp);
+    }
+    else temp = fibo(n-1, 0)+fibo(n-2, 0);
     return temp;
 }
 
@@ -18,6 +23,6 @@ void main() {
     int n;
     printf("How many recursive numbers do you want to find? ");
     scanf("%d",&n);
-    printf("The numbers are: 1 \t");
-    fibo(n);
+    printf("The numbers are: \t");
+    fibo(n, 1);
 }
