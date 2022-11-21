1e9e08e5d444486327db69d89bc488487f3e4540
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2022-11-21 21:38:25 +0545

    refactor code

--- 3rd sem/DSA/recursive-fibo.c
+++ 3rd sem/DSA/recursive-fibo.c
@@ -3,19 +3,14 @@
 int fibo(int n, int disp) {
     int temp;
     if (n==1) {
-        if (disp==1) {
-            printf("1 \t");
-        }
+        if (disp==1) printf("1 \t");
         return 1; 
     } 
     else if (n<=0) { 
         return 0;
     }
-    if (disp == 1) {
-        temp = fibo(n-1, 1)+fibo(n-2, 0);
-        printf("%d \t", temp);
-    }
-    else temp = fibo(n-1, 0)+fibo(n-2, 0);
+    temp = fibo(n-1, disp)+fibo(n-2, 0);
+    if (disp == 1) printf("%d \t", temp);
     return temp;
 }
 
