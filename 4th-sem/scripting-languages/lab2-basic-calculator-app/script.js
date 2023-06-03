a7af98e4e482df3cb96ce6fa12f1fc2b379532c8
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2023-06-03 18:51:42 +0545

    finish polished version of scr lab 2

--- 4th-sem/scripting-languages/lab2-basic-calculator-app/script.js
+++ 4th-sem/scripting-languages/lab2-basic-calculator-app/script.js
@@ -1,39 +1,13 @@
-let calculator = document.querySelector('#calculator');
-console.log({calculator});
-
-calculator.querySelectorAll('button').forEach( clickedButton => {
-});
-
-// on any button click, get values
-//
-
-function getInputs() {
+function calculate(symbol) {
     let values = [];
     document.querySelectorAll('input').forEach(input => values.push(parseInt(input.value)));
-    return values;
-}
+    let result = 0;
 
-function displayResult(result) {
-    calculator.querySelector('#result').innerText = result;
-}
-
-function add() {
-    let values = getInputs();
-    displayResult(values[0]+values[1]);
-}
-function subtract() {
-    let values = getInputs();
-    displayResult(values[0]-values[1]);
-}
-function multiply() {
-    let values = getInputs();
-    displayResult(values[0]*values[1]);
-}
-function divide() {
-    let values = getInputs();
-    displayResult(Math.trunc(values[0]/values[1]));
-}
-function remainder() {
-    let values = getInputs();
-    displayResult(values[0]%values[1]);
+    result = (symbol === '+') ? values[0] + values[1]
+           : (symbol === '-') ? values[0] - values[1]
+           : (symbol === '*') ? values[0] * values[1]
+           : (symbol === '/') ? parseInt(values[0] / values[1])
+           : (symbol === '%') ? values[0] % values[1]
+           : 0;
+    document.querySelector('#result').innerText = `${values[0]} ${symbol} ${values[1]} = ${result}`;
 }
