9ab64bea8fc50b30ca3d24d6808619bc83788037
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2023-12-15 18:52:13 +0545

    dot net: rename folder; add today's

---
+++ 5th-sem/dot-net-technology/2023-12-15/multiplicationTable.cs
@@ -0,0 +1,43 @@
+using System;
+public class Program {
+    public static void Main(string[] args) {
+        /*
+        Console.Write("Enter a number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+        int i=1;
+        do {
+            Console.WriteLine($"{n} * {i} = {n*i}");
+            i++;
+        } while(i<=10);
+
+        //foreach
+
+        int[] arr = {1,2,3,4};
+        foreach(int x in arr) {
+            Console.WriteLine(x);
+        }
+        */
+
+        /*
+        Console.Write("Enter a string ");
+        string str = Console.ReadLine();
+        Console.WriteLine("1st char is: {0}", str[0]);
+        Console.WriteLine("Characters are:");
+        foreach(char c in str) {
+            Console.WriteLine(c);
+        }
+        */
+
+        public void EvenGoto() {
+            /*
+            int n;
+Input: 
+            Console.WriteLine("Enter an even number");
+            n = Convert.ToInt32(Console.ReadLine());
+            if(n % 2 != 0) goto Input;
+            else Console.WriteLine("You have entered an even number");
+            */
+        }
+    }
+}
